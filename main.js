536e88be680f23c3a4e8b6747af66607f2df80f8
barrerasantino7 <santinobarrera1@gmail.com>
2024-01-08 14:07:38 -0300

    Listo desadio

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Hola mundo")
+class ProductManager {
+    constructor(){
+        this.products = []
+    }
+     static id = 0;
+
+     getProduct(){
+         return this.products;
+     }
+
+    addProducts(title, description, price, thumbnail, code, stock){
+
+        if(this.products.find((products)=>products.code === code)){
+            console.log("ERROR")
+        }else{
+            ProductManager.id++
+            this.products.push({id:ProductManager.id, title, description, price, thumbnail, code, stock})
+        }
+    }
+
+    getProductById(id){
+        if(this.products.find((products)=> products.id === id)){
+            console.log("Product found")
+        }else{
+            console.log("Not found")
+        }
+    }
+
+}
+
+
+
+
