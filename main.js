dc3f2b09e3f1f60ab9605b5f55bd7b45504bdd07
barrerasantino7 <santinobarrera1@gmail.com>
2024-01-23 20:29:22 -0300

    desafio-3

--- main.js
+++ main.js
@@ -1,33 +1,57 @@
+const fs = require("fs");
 class ProductManager {
-    constructor(){
+    constructor(path){
         this.products = []
+        this.path = path;
     }
      static id = 0;
 
-     getProduct(){
-         return this.products;
+     async getProduct(){
+        const read = await fs.promises.readFile(this.path, "utf-8");
+        const readJson = JSON.parse(read)
+
+        return readJson;
      }
 
-    addProducts(title, description, price, thumbnail, code, stock){
-
+    async addProducts(title, description, price, thumbnail, code, stock){
         if(this.products.find((products)=>products.code === code)){
             console.log("ERROR")
         }else{
             ProductManager.id++
             this.products.push({id:ProductManager.id, title, description, price, thumbnail, code, stock})
+
+            const productJson = JSON.stringify(this.products, null, 2)
+
+            await fs.promises.writeFile(this.path, productJson);
         }
     }
 
-    getProductById(id){
-        if(this.products.find((products)=> products.id === id)){
-            console.log("Product found")
+    async getProductById(id){
+        const products = await this.getProduct();
+        const product = products.find((products)=> products.id === id)
+
+        if(product){
+            return product
         }else{
-            console.log("Not found")
+            console.log("Producto no encontrado")
         }
     }
 
-}
+    async deleteProdcutById(id){
+        let deleteProducts = await this.getProduct();
+        deleteProducts = deleteProducts.filter((product)=> product.id !== id);
+        const newArray = JSON.stringify(deleteProducts, null, 2)
+        await fs.promises.writeFile(this.path, newArray);
 
+        return newArray; 
+    }
+
+    async updateProduct(id,updateCamp){
+        const products = this.getProduct();
+        const productById = products.find((products)=> products.id === id);
+
+    }
+}
 
 
 
